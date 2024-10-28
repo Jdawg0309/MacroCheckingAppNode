0ad178ca31249a77175c765708ffbb666b264ad0
Junaet Mahbub <52060704+Jdawg0309@users.noreply.github.com>
2024-10-28 15:05:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.set('view engine', 'ejs')
+// Middleware to serve static files
+app.use(express.static('public'));
 
+// Middleware to parse URL-encoded bodies (from HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies (in case you need it)
+app.use(express.json());
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Import your router
+const userRouter = require('./routes/objects');
+
+// Route to render the home page
 app.get('/', (req, res) => {
-    console.log(__dirname)
-    res.render('index')
-})
+    res.render('index');
+});
 
-app.listen(3000)
+// Use the router for /objects routes
+app.use('/objects', userRouter);
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
